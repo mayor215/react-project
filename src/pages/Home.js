ac3e81ff0fb421a8d31a1513bcf263ed4a7a1301
mayor215 <mebukail85@gmail.com>
2023-06-29 11:52:28 +0100

    added link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom"
+
+
 export default function Home(){
    return<>
       <h1 className="text-center m-5">REACT PROJECTS</h1>
    <div className="projects d-flex flex-column align-items-center">
-    <a href="/todo-app" className="project">Todo App</a>
-    <a href="/expense-tracker" className="project">Expense Tracker</a>
-    <a href="/fake-store" className="project">Fake Store</a>
-    <a href="/movie-website" className="project">Movie Website</a>
+    <Link to="/todo-app" className="project">Todo App</Link>
+    <Link to="/expense-tracker" className="project">Expense Tracker</Link>
+    <Link to="/fake-store" className="project">Fake Store</Link>
+    <Link to="/movie-website" className="project">Movie Website</Link>
    </div>
    </>   
 }
